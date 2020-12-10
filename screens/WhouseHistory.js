fee0e07313d7a93e80899d3be67eb0ebc4a0c494
huy1999dnbk <minhhuy99dn@gmail.com>
2020-12-10 11:36:13 +0700

    done histories warehouse

--- screens/WhouseHistory.js
+++ screens/WhouseHistory.js
@@ -1,6 +1,6 @@
 import AsyncStorage from '@react-native-community/async-storage';
 import React, { useState, useEffect } from 'react';
-import { View, StyleSheet, Text, FlatList } from 'react-native';
+import { View, StyleSheet, Text, FlatList, Image } from 'react-native';
 
 
 const WhouseHistory = () => {
@@ -39,10 +39,18 @@ const WhouseHistory = () => {
                 renderItem={({ item }) => {
                     return (
                         <View style={styles.card}>
-                            <Text style={styles.text}>{item.products[0] ? item.products[0].name : "null" }</Text>
-                            <Text style={styles.text}>{item.date}</Text>
-                            <Text style={styles.text}>{item.users[0] ? item.users[0].name : "null"}</Text>
-                            <Text style={styles.text}>{item.note}</Text>
+                            <View style={styles.image}>
+                                <Image style={styles.tinyLogo} source={{
+                                    uri: item.products[0] ? item.products[0].image : "null"
+                                }} />
+                            </View>
+                            <View style={styles.info}>
+                                <Text style={styles.text}>{item.products[0] ? item.products[0].name : "null"}</Text>
+                                <Text style={styles.text}>{item.date}</Text>
+                                <Text style={styles.text}>{item.users[0] ? item.users[0].name : "null"}</Text>
+                                <Text style={styles.text}>{item.note}</Text>
+                            </View>
+
                         </View>
                     );
                 }}
@@ -54,15 +62,42 @@ const WhouseHistory = () => {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
+        backgroundColor:'#fff',
     },
     card: {
         flex: 1,
-        height: 200,
-        borderWidth: 2,
-        borderColor: 'black'
+        height: 180,
+        flexDirection:'row',
+        marginBottom:20,
+        marginHorizontal:10,
+        borderWidth:2,
+        borderRadius:10,
+        borderColor:'#0d023d'
+    },
+    image:{
+        flex:1,
+        justifyContent:'center',
+        alignItems:'center',
+    },
+    info:{
+        flex:2,
+        justifyContent:'center',
+        alignItems:'flex-start',
+        paddingLeft:20
     },
     text: {
-        color: 'black'
+        color:'#1f1f3d',
+        fontFamily:'Roboto-Bold',
+        fontSize:14,
+        textTransform:'uppercase',
+        
+    },
+    tinyLogo:{
+        width:120,
+        height:120,
+        borderRadius:60,
+        borderColor:'black',
+        borderWidth:1
     }
 });
 
